496bb7bc9f71c3f8be026ff7db8a3d5cf91ccbaa
miteshdhiran <mitesh_dhiran@hotmail.com>
2022-09-19 12:54:42 +0530

    Used strongly typed input and output message

--- DistributedMessagePassing/Program.fs
+++ DistributedMessagePassing/Program.fs
@@ -62,30 +62,72 @@ type 'a msg =
    
 let print s = System.Console.WriteLine((s()).ToString())    
 
-// let ipAddress, port = IPAddress.Parse "192.168.1.5", 8001
+
+type MessagePassing<'a, 'b> =
+    static member Client(ipAddress: IPAddress, port) : result<('a -> result<unit>) * IEvent<'b>> =
+        try
+            let client = new Sockets.TcpClient(NoDelay=true)
+            client.Connect(ipAddress, port)
+            let stream = client.GetStream()
+            let send, receive = streamConnection stream
+            Success(send, receive)
+        with _ -> Failure
+    
+    static member Server port : result<('b -> unit) * IEvent<'a>> =
+        try
+            let server = Sockets.TcpListener(IPAddress.Any, port)
+            server.Start()
+            let relay = Event<_>()
+            let received = Event<_>()
+            async {
+                while true do
+                    let client = server.AcceptTcpClient()
+                    let stream = client.GetStream()
+                    let sendToClient, receivedFromClient = streamConnection stream
+                    receivedFromClient.Add received.Trigger
+                    let rec handler = Handler<_>(fun _ msg ->
+                        match sendToClient msg with
+                        | Success() -> ()
+                        | Failure -> relay.Publish.RemoveHandler handler)
+                    relay.Publish.AddHandler handler
+            } |> Async.Start
+            Success(relay.Trigger, received.Publish)
+            with _ -> Failure
+
+
+//sample
+// input output message definition
+type 'a imsg =
+    | Hello of 'a
+
+type 'a omsg =
+    | World of 'a    
+
+type ExampleMessagePassing = MessagePassing<int imsg, int omsg>
+
 let ipAddress, port = IPAddress.Loopback, 8001
-print (fun () -> "Connecting to server..." + ipAddress.ToString())
 
 do
-    match serverConnection port with
-            | Failure -> failwith "Failed to start server"
-            | Success(relayToAllClients, receivedFromClient) ->
-                    let serverHandler msg =
-                        print(fun () -> sprintf "Server received: %A" msg)
-                        match msg with
-                        | Hello n -> relayToAllClients (World (n+1))
-                        | World _ -> ()
-                    receivedFromClient.Add serverHandler
-                    match clientConnection (ipAddress, port) with
-                    | Failure -> failwith "Client failed to connect to server"
-                    | Success(sendToServer, receivedFromServer) ->
-                            let clientHandler msg =
-                                print(fun () -> sprintf "Client received: %A" msg)
-                            receivedFromServer.Add clientHandler
-                            match sendToServer (Hello 3) with
-                            | Failure -> failwith "Client failed to send message to server"
-                            | Success() -> ()
+    match ExampleMessagePassing.Server port with
+    | Failure -> failwith "Failed to start server"
+    | Success(relayToAllClients, receivedFromClient) ->
+        let serverHandler msg =
+            print(fun () -> sprintf "Server received: %A" msg)
+            match msg with
+            | Hello n -> relayToAllClients (World (n+1))
+        receivedFromClient.Add serverHandler
+        
+        match ExampleMessagePassing.Client (ipAddress, port) with
+        | Failure -> failwith "Client failed to connect to server"
+        | Success(sendToServer, receivedFromServer) ->
+            let clientHandler msg =
+                print(fun () -> sprintf "Client received: %A" msg)
+            receivedFromServer.Add clientHandler
+            match sendToServer (Hello 3) with
+            | Failure -> failwith "Client failed to send message to server"
+            | Success() -> ()
 
-print (fun () -> "Done") 
+    
+print (fun () -> "Press ENTER to exit") 
 stdin.ReadLine() |> ignore
                     
